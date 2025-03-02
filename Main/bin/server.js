4be6e7edca0b8a120b67db0c10add2f80c6ab14f
bthntncr81 <93138181+bthntncr81@users.noreply.github.com>
2025-03-02 23:37:18 +0300

    changes

--- Main/bin/server.js
+++ Main/bin/server.js
@@ -1,24 +1,34 @@
 import 'reflect-metadata';
 import { Ignitor, prettyPrintError } from '@adonisjs/core';
+import { createServer } from 'node:http';  // Change to HTTP
+
 const APP_ROOT = new URL('../', import.meta.url);
+
 const IMPORTER = (filePath) => {
-    if (filePath.startsWith('./') || filePath.startsWith('../')) {
-        return import(new URL(filePath, APP_ROOT).href);
-    }
-    return import(filePath);
+  if (filePath.startsWith('./') || filePath.startsWith('../')) {
+    return import(new URL(filePath, APP_ROOT).href);
+  }
+  return import(filePath);
 };
+
+// Start the application
 new Ignitor(APP_ROOT, { importer: IMPORTER })
-    .tap((app) => {
+  .tap((app) => {
     app.booting(async () => {
-        await import('#start/env');
+      await import('#start/env');
     });
     app.listen('SIGTERM', () => app.terminate());
     app.listenIf(app.managedByPm2, 'SIGINT', () => app.terminate());
-})
-    .httpServer()
-    .start()
-    .catch((error) => {
+  })
+  .httpServer()
+  .start()
+  .then((app) => {
+    // Use HTTP instead of HTTPS here
+    createServer(app).listen(8001, () => {
+      console.log('Adonis.js HTTP server is running on port 8001');
+    });
+  })
+  .catch((error) => {
     process.exitCode = 1;
     prettyPrintError(error);
-});
-//# sourceMappingURL=server.js.map
+  });
