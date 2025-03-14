8a8336d42de1473ea459d1141160e576f06110cf
bthntncr81 <93138181+bthntncr81@users.noreply.github.com>
2025-03-14 18:02:51 +0300

    product choose change

--- inertia/components/Customer/Home/ProductDetails.tsx
+++ inertia/components/Customer/Home/ProductDetails.tsx
@@ -375,7 +375,7 @@ export default function ProductDetails({
                           <Td className="text-sm" w="150px">
                             <QuantityController
                               stopSign={0}
-                              defaultValue={1}
+                              defaultValue={0}
                               disabled={!addon.isAvailable}
                               value={addonsSelected.find((v) => v.id === addon.id)?.quantity || 0}
                               onValueChange={(qnt) =>
